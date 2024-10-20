fdaeef3b69f148eecf0e6fa498e619c1a5ebe751
jungyeon <wjddus5533@naver.com>
2024-10-20 14:04:10 +0900

    회원 엔티티 구조 도메인형으로 수정

--- src/main/java/com/spinner/www/users/entity/Users.java
+++ src/main/java/com/spinner/www/users/entity/Users.java
@@ -1,4 +1,4 @@
-package com.spinner.www.entity;
+package com.spinner.www.users.entity;
 
 import jakarta.persistence.*;
 
--- src/main/java/com/spinner/www/users/repository/UserRepo.java
+++ src/main/java/com/spinner/www/users/repository/UserRepo.java
@@ -1,6 +1,6 @@
-package com.spinner.www.repository;
+package com.spinner.www.users.repository;
 
-import com.spinner.www.entity.Users;
+import com.spinner.www.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
