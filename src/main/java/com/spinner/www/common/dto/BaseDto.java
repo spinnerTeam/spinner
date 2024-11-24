850851b1891d78fb731b5338e488eb8e09c59c52
hyper <revoluer9@gmail.com>
2024-11-24 13:35:48 +0900

    feat: BaseDto 수정, 애플리케이션 프로퍼티 주석 추가

--- src/main/java/com/spinner/www/common/dto/BaseDto.java
+++ src/main/java/com/spinner/www/common/dto/BaseDto.java
@@ -1,18 +1,11 @@
 package com.spinner.www.common.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class BaseDto {
-
     private String createdAt;
     private LocalDateTime createdDate;
     private String modifiedAt;
