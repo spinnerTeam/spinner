1635ee2a276d8ab70b3d7ddb9d351d829bfb4fa1
jungyeon <wjddus5533@naver.com>
2024-11-25 22:49:50 +0900

    feat : 소설로그인 추가
    - 소셜로그인 (네이버, 카카오, 구글) 연동 완료
    - 서비스 로직 미적용

---
+++ src/main/java/com/spinner/www/member/repository/SocialRepo.java
@@ -0,0 +1,16 @@
+package com.spinner.www.member.repository;
+
+import com.spinner.www.member.entity.Social;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SocialRepo extends JpaRepository<Social, Long> {
+
+    /**
+     * sub 유무
+     * @param sub String
+     * @return boolean
+     */
+    boolean existsBySocialSub(String sub);
+}
