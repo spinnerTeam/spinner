ed891ef411c0e3fa26857b895e5f0167d16120f3
hyper <revoluer9@gmail.com>
2024-11-28 18:55:53 +0900

    feat: report io validation 추가

--- src/main/java/com/spinner/www/report/io/ReportCreateRequest.java
+++ src/main/java/com/spinner/www/report/io/ReportCreateRequest.java
@@ -1,11 +1,14 @@
 package com.spinner.www.report.io;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class ReportCreateRequest {
+    @NotBlank(message = "신고 타입은 필수 입력 조건입니다.")
     private Long reportTypeIdx;
+    @NotBlank(message = "게시물 넘버는 필수 입력 조건입니다.")
     private Long postIdx;
 }
