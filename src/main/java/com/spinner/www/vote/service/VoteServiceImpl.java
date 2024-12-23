39ad645000a321f6833e905c359ce9c64a226191
hyper <revoluer9@gmail.com>
2024-12-24 02:51:53 +0900

    feat: Vote 전체 리스트 조회 재수정

--- src/main/java/com/spinner/www/vote/service/VoteServiceImpl.java
+++ src/main/java/com/spinner/www/vote/service/VoteServiceImpl.java
@@ -101,8 +101,6 @@ public class VoteServiceImpl implements VoteService {
         // 커뮤니티는 투표 완료가 없으며, 투표한 사람만 결과 확인이 가능함
         // 투표 상태에 따라 기본 베이스가 변경됨 (ing, multiple 항목, end 결과)
         VoteSelectDto voteSelectDto = voteMapper.toVoteSelectDto(boardIdx);
-        log.info("VoteSelectDto: {}", voteSelectDto);
-        log.info("VoteSelectDto.boardIdx: {}", voteSelectDto.getBoardIdx());
 
         Board board = boardRepo.getReferenceById(voteSelectDto.getBoardIdx());
 
@@ -113,13 +111,12 @@ public class VoteServiceImpl implements VoteService {
             return new ResponseEntity<>(ResponseVOUtils.getFailResponse(CommonResultCode.DATA_NOT_FOUND), HttpStatus.NOT_FOUND);
         }
 
-        for (Vote vote : votes) {
+        List<Object> voteResponseList = new ArrayList<>();
+        VoteSelectResponse voteSelectResponse;
 
+        for (Vote vote : votes) {
             // (1) 투표가 진행 중이거나 중복 투표가 가능하면
             if (vote.getVoteStatus() == VoteStatus.ing || vote.getVoteStatus() == VoteStatus.multiple) {
-
-                VoteSelectResponse voteSelectResponse;
-
                 // (1-1) 로그인 여부 확인
                 Long memberIdx = sessionInfo.getMemberIdx();
 
@@ -131,25 +128,26 @@ public class VoteServiceImpl implements VoteService {
                     if (voteUser != null) {
                         // (1-3) 유저가 투표에 참여했으면
                         voteSelectResponse = voteCustomMapper.createVoteSelectResponse(vote, votes, true);
+                        voteResponseList.add(voteSelectResponse);
                     } else {
                         // (1-4) 유저가 투표에 참여하지 않았으면
                         voteSelectResponse = voteCustomMapper.createVoteSelectResponse(vote, votes, false);
+                        voteResponseList.add(voteSelectResponse);
                     }
                 } else {
                     // (1-5) 비로그인 사용자 처리
                     voteSelectResponse = voteCustomMapper.createVoteSelectResponse(vote, votes, false);
+                    voteResponseList.add(voteSelectResponse);
                 }
-
-                return new ResponseEntity<>(ResponseVOUtils.getSuccessResponse(voteSelectResponse), HttpStatus.OK);
 
             // (2) 투표가 완료됐으면
             } else {
                 VoteResultsResponse voteResultsResponse = voteQueryRepo.findVoteResultsByVote(vote);
-                return new ResponseEntity<>(ResponseVOUtils.getSuccessResponse(voteResultsResponse), HttpStatus.OK);
+                voteResponseList.add(voteResultsResponse);
             }
         }
 
-        return new ResponseEntity<>(ResponseVOUtils.getFailResponse(CommonResultCode.DATA_NOT_FOUND), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(ResponseVOUtils.getSuccessResponse(voteResponseList), HttpStatus.OK);
     }
 
     /**
