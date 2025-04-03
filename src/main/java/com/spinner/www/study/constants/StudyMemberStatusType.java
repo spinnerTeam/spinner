83d9508a8d724676dfef1589ef03e040aa083869
kjy <jungyeon@newpermarket.com>
2025-04-03 15:51:47 +0900

    chore : 사용 안하는 스터디 패키지 삭제

--- src/main/java/com/spinner/www/study/constants/StudyMemberStatusType.java
+++
@@ -1,6 +0,0 @@
-package com.spinner.www.study.constants;
-
-public enum StudyMemberStatusType {
-    // 대기, 가입, 거절, 강퇴
-    WAITING, JOIN, DISAPPROVE, KICK
-}
--- src/main/java/com/spinner/www/study/constants/StudySortType.java
+++
@@ -1,5 +0,0 @@
-package com.spinner.www.study.constants;
-
-public enum StudySortType {
-    desc, like
-}
