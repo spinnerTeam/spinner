bef7eed1a7e3c344724d600a830625186f701454
kjy <jungyeon@newpermarket.com>
2025-04-04 12:55:52 +0900

    chore : cors 수정

--- src/main/java/com/spinner/www/config/WebConfig.java
+++ src/main/java/com/spinner/www/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
