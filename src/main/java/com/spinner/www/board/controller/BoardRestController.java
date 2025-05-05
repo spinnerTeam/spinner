0085acc68528864390840b227ee9fcd58948cd84
sujin <nataya1116@gmail.com>
2025-05-05 17:16:31 +0900

    refactor: 인기글 엔드포인트 변경 /home/hot/board/{boardType} -> /board/hot/{boardType}

--- src/main/java/com/spinner/www/board/controller/BoardRestController.java
+++ src/main/java/com/spinner/www/board/controller/BoardRestController.java
@@ -129,7 +129,7 @@ public class BoardRestController {
     }
 
     /**
-     * (스터디)게시글 삭제
+     * 게시글 삭제
      * @param boardType String
      * @param boardIdx Long 게시글 idx
      * @return ResponseEntity<CommonResponse> 삭제 응답 결과
@@ -151,5 +151,29 @@ public class BoardRestController {
     public ResponseEntity<CommonResponse> delete(@PathVariable("boardType") String boardType,
                                                  @PathVariable("boardIdx") Long boardIdx) {
         return boardService.delete(boardType, boardIdx);
+    }
+
+    /**
+     * 인기 게시글 목록 조회
+     * @param boardType String
+     * @param idx Long 조회 시작 idx
+     * @param size int 조회할 목록 갯수
+     * @return searchParamRequest<CommonResponse> 게시글 목록
+     */
+    @Operation(description = "게시글 목록을 조회합니다. <br/>" +
+            "해당 idx부터 size까지 게시글의 목록을 출력합니다 <br/>" +
+            "keyword에 값이 있을 시 keyword에 해당하는 제목과 작성자를 출력합니다.<br/><br/>" +
+            "<strong>[boardType]</strong> <br/>" +
+            "verify : 공부인증글 <br/>" +
+            "free   : 자유글",
+            responses = {
+                    @ApiResponse(content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "20000", description = "요청 성공"),
+            })
+    @GetMapping("/hot/{boardType}")
+    public ResponseEntity<CommonResponse> findByAll(@PathVariable("boardType") String boardType,
+                                                    @RequestParam(value = "idx", required = false) Long idx,
+                                                    @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
+        return boardService.getSliceOfHotBoard(boardType, idx, size);
     }
 }
