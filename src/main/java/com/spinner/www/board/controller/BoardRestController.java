b1b4ea72c78ebb8d56f2fffb9e9f1cad0f7412d8
sujin <nataya1116@gmail.com>
2025-05-05 18:21:09 +0900

    style: 게시판 컨트롤러 주석 수정(일반 게시판에는 공지사항 없어서 주석에서 제거)

--- src/main/java/com/spinner/www/board/controller/BoardRestController.java
+++ src/main/java/com/spinner/www/board/controller/BoardRestController.java
@@ -34,7 +34,6 @@ public class BoardRestController {
             "<strong>[boardType]</strong> <br/>" +
             "verify : 공부인증글 <br/>" +
             "free   : 자유글 <br/>" +
-            "notice : 공지사항 <br/>" +
             "게시글의 img, video 태그 src 속성에 업로드할 이미지와 비디오의 오리지널 파일명을 넣어주셔야 태그에서 사용 가능합니다. <br/>" +
             "ex) 파일명이 \"Group+7@2x (1).png\"이면 &lt;img src=\"Group+7@2x (1).png\"/&gt;로 처리",
 //            "",
@@ -61,8 +60,7 @@ public class BoardRestController {
             "idx(pk)에 해당하는 게시글 정보를 반환합니다. <br/><br/>" +
             "<strong>[boardType]</strong> <br/>" +
             "verify : 공부인증글 <br/>" +
-            "free   : 자유글 <br/>" +
-            "notice : 공지사항 <br/>" ,
+            "free   : 자유글 <br/>" ,
             responses = {
                     @ApiResponse(content = @Content(mediaType = "application/json")),
                     @ApiResponse(responseCode = "20000", description = "요청 성공"),
@@ -87,8 +85,7 @@ public class BoardRestController {
             "keyword에 값이 있을 시 keyword에 해당하는 제목과 작성자를 출력합니다.<br/><br/>" +
             "<strong>[boardType]</strong> <br/>" +
             "verify : 공부인증글 <br/>" +
-            "free   : 자유글 <br/>" +
-            "notice : 공지사항 <br/>" ,
+            "free   : 자유글 <br/>" ,
             responses = {
                     @ApiResponse(content = @Content(mediaType = "application/json")),
                     @ApiResponse(responseCode = "20000", description = "요청 성공"),
@@ -112,8 +109,7 @@ public class BoardRestController {
             "해당 idx의 제목과 내용을 수정합니다<br/><br/>" +
             "<strong>[boardType]</strong> <br/>" +
             "verify : 공부인증글 <br/>" +
-            "free   : 자유글 <br/>" +
-            "notice : 공지사항 <br/>" ,
+            "free   : 자유글 <br/>",
             responses = {
                     @ApiResponse(content = @Content(mediaType = "application/json")),
                     @ApiResponse(responseCode = "20000", description = "요청 성공"),
@@ -138,8 +134,7 @@ public class BoardRestController {
             "해당 idx의 게시글을 삭제합니다. <br/><br/>" +
             "<strong>[boardType]</strong> <br/>" +
             "verify : 공부인증글 <br/>" +
-            "free   : 자유글 <br/>" +
-            "notice : 공지사항 <br/>" ,
+            "free   : 자유글 <br/>" ,
             responses = {
                     @ApiResponse(content = @Content(mediaType = "application/json")),
                     @ApiResponse(responseCode = "20000", description = "요청 성공"),
